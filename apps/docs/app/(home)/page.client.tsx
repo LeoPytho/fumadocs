c401269ecedd1aa0aa92ba84f1b714a659ec254e
LeoPytho <108222261+LeoPytho@users.noreply.github.com>
2025-06-28 21:29:30 +0700

    Update page.client.tsx

--- apps/docs/app/(home)/page.client.tsx
+++ apps/docs/app/(home)/page.client.tsx
@@ -16,7 +16,7 @@ import { cn } from '@/lib/cn';
 import { buttonVariants } from '@/components/ui/button';
 import Image from 'next/image';
 import MainImg from './20250628_211230.png'; // Replace with an image showing your JKT48Connect powered app
-import OpenAPIImg from './openapi.png'; // Replace with an image showing your JKT48Connect API docs/playground
+import OpenAPIImg from './20250628_211911.png'; // Replace with an image showing your JKT48Connect API docs/playground
 import { cva } from 'class-variance-authority';
 
 export function CreateAppAnimation() {
