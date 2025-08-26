2e3cdd9334041d1d134f7957c705898b5cca4342
LeoPytho <108222261+LeoPytho@users.noreply.github.com>
2025-08-26 22:48:56 +0700

    Update layout.tsx

--- apps/docs/app/(home)/layout.tsx
+++ apps/docs/app/(home)/layout.tsx
@@ -37,6 +37,11 @@ export default function Layout({ children }: { children: ReactNode }) {
               url: '/docs/openapi',
               icon: <ComponentIcon />,
             },
+            {
+              text: 'Changelog',
+              url: '/changelog',
+              icon: <ComponentIcon />,
+            },
           ],
         },
         {
